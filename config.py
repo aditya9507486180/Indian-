5280192e45eab46118d2aed87a8911d1a91fc3d5
aditya9507486180 <firefox95074.8@gmail.com>
2024-09-03 21:01:06 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7427052085:AAGHBfZ92GuLI47szb_86hVV0kAxEUY_t2w")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("ADITYAx09")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6503409388")
+
+
+
+
+
+
+
+  
